8d07ce7a08c013bb521e226f7b3a72bb46b23bd9
zhoubohan <m18618474796@163.com>
2018-05-14 06:41:13 +0000

    add laravel mix

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            不积跬步无以至千里。
+        </p>
+        <p class="pull-right"><a href="455093504@qq.com"></a>联系我们</p>
+    </div>
+</footer>
